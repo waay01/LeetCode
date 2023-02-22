f3fed86f74adca23d9004738cdb33cbf67443f56
waay <exc3ls0@mail.ru>
2023-02-22 16:36:22 +0300

    feat: add Add Two Integers

---
+++ Algorithms/Easy/2235. Add Two Integers.cpp
@@ -0,0 +1,14 @@
+#include "../includes.h"
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+};
+
+int main() {
+    Solution Solution;
+
+    Solution.sum(-31, 2);
+}
